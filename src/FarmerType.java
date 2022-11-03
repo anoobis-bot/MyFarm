b008f82799dfd77e76f13679bce7dd3f9a5d5a91
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-11-03 11:37:13 +0800

    Created getter methods for FarmerType

--- src/FarmerType.java
+++ src/FarmerType.java
@@ -16,4 +16,24 @@ public class FarmerType
         this.bonusLimitIncrease = farmerAttributes.bonusLimitIncrease;
         this.bonusFertilizeIncrease = farmerAttributes.bonusFertilizeIncrease;
     }
+
+    public String getNameType() {
+        return nameType;
+    }
+
+    public int getBonusCoin() {
+        return bonusCoin;
+    }
+
+    public int getSeedReductionCost() {
+        return seedReductionCost;
+    }
+
+    public int getBonusLimitIncrease() {
+        return bonusLimitIncrease;
+    }
+
+    public int getBonusFertilizeIncrease() {
+        return bonusFertilizeIncrease;
+    }
 }
