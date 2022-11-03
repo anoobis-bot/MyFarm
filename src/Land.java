f91786d58927f5a9f119d2964e5eb3c4d19db39b
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-11-03 12:16:11 +0800

    Created Land class

---
+++ src/Land.java
@@ -0,0 +1,50 @@
+import Constants.SeedAttributes;
+
+public class Land
+{
+    private Seed crop;
+    private int amtWater;
+    private int amtFertilizer;
+
+    Land()
+    {
+        this.amtWater = 0;
+        this.amtFertilizer = 0;
+    }
+
+    public void setSeed(SeedAttributes seed)
+    {
+        this.crop = new Seed(seed);
+    }
+
+    public Seed getCurrentSeed()
+    {
+        return this.crop;
+    }
+
+    public int getAmtWater()
+    {
+        return amtWater;
+    }
+
+    public int getAmtFertilizer()
+    {
+        return amtFertilizer;
+    }
+
+    public void waterLand()
+    {
+        this.amtWater++;
+    }
+
+    public void fertilizeLand()
+    {
+        this.amtFertilizer++;
+    }
+
+    public void resetValues()
+    {
+        this.amtWater = 0;
+        this.amtFertilizer = 0;
+    }
+}
