b84bb36ed37457b1f5ef3b82425c955c50932ec3
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-11-03 09:42:14 +0800

    Created Point class

---
+++ src/Point.java
@@ -0,0 +1,25 @@
+public class Point
+{
+    private int xCoordinate;
+    private int yCoordinate;
+
+    public int getXCoordinate()
+    {
+        return xCoordinate;
+    }
+
+    public void moveXCoordinate(int xCoordinate)
+    {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate()
+    {
+        return yCoordinate;
+    }
+
+    public void moveYCoordinate(int yCoordinate)
+    {
+        this.yCoordinate = yCoordinate;
+    }
+}
